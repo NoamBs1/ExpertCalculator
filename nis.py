bd17f3d2f2e3bdbd8dd35b27d9bb7d6dff459616
Noam Bs <noambenshoshan104@gmail.com>
2024-01-09 11:32:18 +0200

    the algorithm class modified

--- nis.py
+++ nis.py
@@ -2,11 +2,19 @@ from inputChecks import *
 from mathEvaluation import MathEvaluation
 from Operator import Operator
 
+POST = 1
+PRE = -1
+
 
 class Algorithm:
 
     def __init__(self, operators: dict):
         self.operators = operators
+        self.UNARYS = [op for op in self.operators if not self.operators[op].isBinary]
+        self.POSTFIX_UNARYS = [op for op in self.operators if self.operators[op].getPosition() == 1]
+        self.PREFIX_UNARYS = [op for op in self.operators if self.operators[op].getPosition() == -1]
+        self.MINUS_UNARY = 'N'
+        self.operators[self.MINUS_UNARY] = Operator('N', 7, MathEvaluation.neg, False, PRE)
 
     @staticmethod
     def is_numeric(token):
@@ -16,14 +24,18 @@ class Algorithm:
         except ValueError:
             return False
 
+    def is_higher(self, op1, op2):
+        if op1 == self.MINUS_UNARY and op2 in ['^', '!', '$', '@', '&']:
+            return False
+        if self.operators[op1].getKdimut() >= self.operators[op2].getKdimut():
+            return True
+
     def infix_to_postfix(self, expression):
         postfix = []
         stack = []
 
         for token in expression:
-            if token.startswith('-') and Algorithm.is_numeric(token[1:]):
-                postfix.append(token)
-            elif Algorithm.is_numeric(token):
+            if Algorithm.is_numeric(token):
                 postfix.append(token)
             elif token == '(':
                 stack.append(token)
@@ -35,8 +47,7 @@ class Algorithm:
                 else:
                     raise ValueError("not matching parentheses")
             else:
-                while (stack and stack[-1] != '(' and
-                       self.operators[stack[-1]].getKdimut() >= self.operators[token].getKdimut()):
+                while stack and stack[-1] != '(' and self.is_higher(stack[-1], token):
                     postfix.append(stack.pop())
                 stack.append(token)
 
@@ -58,23 +69,27 @@ class Algorithm:
         current_token = ''
 
         for i, char in enumerate(expression):
-            if char.isdigit() or char == '.':
+            if char == self.MINUS_UNARY:
+                raise ValueError(f"Invalid character: {self.MINUS_UNARY}")
+            elif char.isdigit() or (char == '.' and current_token and '.' not in current_token):
                 current_token += char
-            elif ((char in self.operators.keys() or char in ['(', ')']) and
-                  char not in [op for op in self.operators if not self.operators[op].isBinary] + ['-']):
+            elif ((char in self.operators or char in ['(', ')']) and
+                  char not in self.UNARYS + ['-']):
                 if current_token:
                     tokens.append(current_token)
                     current_token = ''
                 tokens.append(char)
             elif char == '-':
-                if i == 0 or expression[i - 1] in self.operators.keys() or expression[i - 1] == '(':
+                if (i == 0 or expression[i - 1] in self.operators and
+                        expression[i - 1] not in self.POSTFIX_UNARYS
+                        or expression[i - 1] == '('):
                     current_token += char
                 else:
                     if current_token:
                         tokens.append(current_token)
                         current_token = ''
                     tokens.append(char)
-            elif char in [op for op in self.operators if self.operators[op].getPosition() == 1]:
+            elif char in self.POSTFIX_UNARYS:
                 if i + 1 < len(expression) and (Algorithm.is_numeric(expression[i + 1]) or expression[i + 1] == '('):
                     raise ValueError(f"{char} should not be followed by a number")
                 else:
@@ -82,8 +97,9 @@ class Algorithm:
                         tokens.append(current_token)
                         current_token = ''
                     tokens.append(char)
-            elif char in [op for op in self.operators if self.operators[op].getPosition() == -1]:
-                if i - 1 > -1 and (Algorithm.is_numeric(expression[i - 1]) or expression[i - 1] == ')'):
+            elif char in self.PREFIX_UNARYS:
+                if i - 1 > -1 and (Algorithm.is_numeric(expression[i - 1]) or expression[i - 1] == ')'
+                                   or expression[i - 1] in self.POSTFIX_UNARYS):
                     raise ValueError(f"{char} should not be preceded by a number")
                 else:
                     if current_token:
@@ -104,6 +120,15 @@ class Algorithm:
                     tokens[i] = token.replace(rep, '')
                 else:
                     tokens[i] = token.replace(rep, '-')
+
+        for i in range(len(tokens)):
+            token = tokens[i]
+            if token.startswith('-') and Algorithm.is_numeric(token[1:]):
+                tokens.insert(i, self.MINUS_UNARY)
+                tokens[i + 1] = tokens[i + 1].replace('-', '')
+            elif token == '-' and tokens[i - 1] not in "0123456789)!":
+                tokens[i] = self.MINUS_UNARY
+        print(tokens)
         return tokens
 
     def calculate_expression(self, expression: list) -> float:
