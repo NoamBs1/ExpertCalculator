350ac9058ed2804d27504fbd5d582e62b7b9e3b1
NoamBs1 <152907579+NoamBs1@users.noreply.github.com>
2024-01-22 13:18:30 +0200

    Add files via upload
    operator class

---
+++ Operator.py
@@ -0,0 +1,29 @@
+class Operator:
+    def __init__(self, char: str, kdimut: int, func, isBinary=True, pos=0):
+        """
+        Initialize a new Operator
+        :param char: str char - the operator
+        :param kdimut: int priority in order of operations
+        :param func: the function that it operates
+        :param isBinary: boolean if the operator is binary
+        :param pos: the position of the operator relative to the location of the operand
+        """
+        self.char = char
+        self.kdimut = kdimut
+        self.func = func
+        self.isBinary = isBinary
+        self.pos = pos
+
+    def getKdimut(self) -> int:
+        """
+        returns the priority of the operator
+        :return: the priority of the operator
+        """
+        return self.kdimut
+
+    def getPosition(self) -> int:
+        """
+        return the position of the operator relative to the location of the operand
+        :return: the position of the operator relative to the location of the operand
+        """
+        return self.pos
